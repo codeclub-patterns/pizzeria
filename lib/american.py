19d5defe125c9f619357dc9521ce52790a492dfe
vlamik19 <madara020498@mail.ru>
2020-07-03 20:06:13 +0300

    Add Americar Pizza

--- lib/american.py
+++ lib/american.py
@@ -2,4 +2,15 @@ from lib.components import AbstractPizza
 
 
 class AmericanPizza(AbstractPizza):
-    pass
+    Name = 'American-pizza'
+
+    def __init__(self, size: int, cost):
+        self._size = size
+        self._cost = cost
+
+    def get_cost(self) -> float:
+        return AbstractPizza.get_cost(self._cost)
+
+    def __str__(self):
+        return f'Name: {AmericanPizza.Name}. \nSize = {self._size}sm. \nCost = {self._cost} uah. '\
+               f'\nComposition: Tomato sauce, marinated chicken, mushrooms, mozzarella, onions, barbecue sauce.'
