d3f68ec95e201052257214e75b0d509170185915
vlamik19 <madara020498@mail.ru>
2020-07-06 17:14:02 +0300

    Переделан клас AmericanPizza

--- lib/american.py
+++ lib/american.py
@@ -2,15 +2,21 @@ from lib.components import AbstractPizza
 
 
 class AmericanPizza(AbstractPizza):
-    Name = 'American-pizza'
 
-    def __init__(self, size: int, cost):
-        self._size = size
-        self._cost = cost
-
-    def get_cost(self) -> float:
-        return AbstractPizza.get_cost(self._cost)
+    def __init__(self, name, size):
+        super().__init__(name, size)
 
     def __str__(self):
-        return f'Name: {AmericanPizza.Name}. \nSize = {self._size}sm. \nCost = {self._cost} uah. '\
-               f'\nComposition: Tomato sauce, marinated chicken, mushrooms, mozzarella, onions, barbecue sauce.'
+        return f'Name: {self._name}\nSize: {self._size} sm'
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def size(self):
+        return self._size
+
+    def get_cost(self):
+        return 100.25
+
--- lib/components.py
+++ lib/components.py
@@ -2,6 +2,17 @@ from abc import ABC, abstractmethod
 
 
 class AbstractPizza(ABC):
+    def __init__(self, name: str, size: int):
+        self._name = name
+        self._size = size
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def size(self):
+        return self._size
 
     @abstractmethod
     def get_cost(self) -> float:
